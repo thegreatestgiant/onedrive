d16be5fd5a64a0f7576c0b695f839d89fb6f5755
The Greatest Giant <seanalter1@gmail.com>
2023-02-05 22:17:52 -0500

    Update FolderListLayout.tsx

--- src/components/FolderListLayout.tsx
+++ src/components/FolderListLayout.tsx
@@ -76,17 +76,6 @@ const FolderListLayout = ({
               indeterminate={true}
               title={t('Select files')}
             />
-            <button
-              title={t('Copy selected files permalink')}
-              className="cursor-pointer rounded p-1.5 hover:bg-gray-300 disabled:cursor-not-allowed disabled:text-gray-400 disabled:hover:bg-white dark:hover:bg-gray-600 disabled:dark:text-gray-600 disabled:hover:dark:bg-gray-900"
-              disabled={totalSelected === 0}
-              onClick={() => {
-                clipboard.copy(handleSelectedPermalink(getBaseUrl()))
-                toast.success(t('Copied selected files permalink.'))
-              }}
-            >
-              <FontAwesomeIcon icon={['far', 'copy']} size="lg" />
-            </button>
             {totalGenerating ? (
               <Downloading title={t('Downloading selected files, refresh page to cancel')} style="p-1.5" />
             ) : (
@@ -118,16 +107,6 @@ const FolderListLayout = ({
 
           {c.folder ? (
             <div className="hidden p-1.5 text-gray-700 dark:text-gray-400 md:flex">
-              <span
-                title={t('Copy folder permalink')}
-                className="cursor-pointer rounded px-1.5 py-1 hover:bg-gray-300 dark:hover:bg-gray-600"
-                onClick={() => {
-                  clipboard.copy(`${getBaseUrl()}${`${path === '/' ? '' : path}/${encodeURIComponent(c.name)}`}`)
-                  toast(t('Copied folder permalink.'), { icon: '👌' })
-                }}
-              >
-                <FontAwesomeIcon icon={['far', 'copy']} />
-              </span>
               {folderGenerating[c.id] ? (
                 <Downloading title={t('Downloading folder, refresh page to cancel')} style="px-1.5 py-1" />
               ) : (
@@ -145,18 +124,6 @@ const FolderListLayout = ({
             </div>
           ) : (
             <div className="hidden p-1.5 text-gray-700 dark:text-gray-400 md:flex">
-              <span
-                title={t('Copy raw file permalink')}
-                className="cursor-pointer rounded px-1.5 py-1 hover:bg-gray-300 dark:hover:bg-gray-600"
-                onClick={() => {
-                  clipboard.copy(
-                    `${getBaseUrl()}/api/raw/?path=${getItemPath(c.name)}${hashedToken ? `&odpt=${hashedToken}` : ''}`
-                  )
-                  toast.success(t('Copied raw file permalink.'))
-                }}
-              >
-                <FontAwesomeIcon icon={['far', 'copy']} />
-              </span>
               <a
                 title={t('Download file')}
                 className="cursor-pointer rounded px-1.5 py-1 hover:bg-gray-300 dark:hover:bg-gray-600"
